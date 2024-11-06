7235f2b7c8374243aa7d06ea8e6f104b3075a775
HeinrichVannas <heinivannas@gmail.com>
2024-11-06 20:45:43 +0200

    add movies to database

--- frontend/vuefrontend/src/views/AppView.vue
+++ frontend/vuefrontend/src/views/AppView.vue
@@ -14,7 +14,7 @@ const searchIcon = SearchIcon;
   async function searchMovies(title) {
     try {
       //movies.value = await (await fetch(title ? `${Local_API}/movie/${title}` : `${Local_API}/movie/`)).json()
-      const response = await fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=a93a45e6&s=${title}`)
+      const response = await fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=a93a45e6&s=${title}`)
       const data = await response.json();
       movies.value = data
       setMovies(data);
